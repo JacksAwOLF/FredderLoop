571e3f54ab43c5e78fd706fa2d238f02e15d63b9
Fred <61068104+P2fryang@users.noreply.github.com>
2024-11-19 21:46:11 -0800

    add reminder notifications

--- discordBot.py
+++ discordBot.py
@@ -10,16 +10,36 @@ def sendDiscordMessage(message):
 
 def createFormMessage():
     formId = getFormId()
-    message = f"New FredderLoop issue just dropped! 3 weeks to add questions here: https://docs.google.com/forms/d/{formId}/edit "
+    message = f"New FredderLoop issue just dropped! 3 weeks (until the 21st) to add questions here: https://docs.google.com/forms/d/{formId}/edit "
     message += "\nAlso, don't forget to set edit responses to true... google form api currently doesn't support setting it in code"
+    sendDiscordMessage(message)
+
+
+def addQuestionsReminderMessage():
+    formId = getFormId()
+    message = (
+        f"Last day to add questions! https://docs.google.com/forms/d/{formId}/edit"
+    )
     sendDiscordMessage(message)
 
 
 def collectResponsesMessage():
     formId = getFormId()
     message = (
-        f"Ready for responses here: https://docs.google.com/forms/d/{formId}/viewform"
+        f"Ready for responses here. Due in a week (the 28th): https://docs.google.com/forms/d/{formId}/viewform"
     )
+    sendDiscordMessage(message)
+
+
+def submissionReminderMessage():
+    formId = getFormId()
+    message = f"Last day to submit your answers (there is NO auto-submit)! https://docs.google.com/forms/d/{formId}/viewform"
+    sendDiscordMessage(message)
+
+
+def lastHourReminderMessage():
+    formId = getFormId()
+    message = f"Only ONE MORE HOUR to submit your answers (there is NO auto-submit)! https://docs.google.com/forms/d/{formId}/viewform"
     sendDiscordMessage(message)
 
 
